b2d570ca7c4d3557a8e6103c6c90ad0dc9f24801
meghna-saxena <meghna.s1906@gmail.com>
2017-10-02 22:10:32 +0530

    handled hoc edge cases

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -2,20 +2,31 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 export default function(ComposedComponent) {
-    class Authentication extends Component {
-        static contextTypes = {
-            router: React.PropTypes.object
-        }
-        
-        render() {
-            console.log(this.context);
-            return <ComposedComponent {...this.props} />
-        }
+  class Authentication extends Component {
+    static contextTypes = {
+      router: React.PropTypes.object
     }
 
-    function mapStateToProps(state) {
-        return {authenticated: state.authenticated};
+    componentWillMount() {
+      if (!this.props.authenticated) {
+        this.context.router.push('/');
+      }
     }
 
-    return connect (mapStateToProps) (Authentication);
+    componentWillUpdate(nextProps) {
+      if (!nextProps.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
+    render() {
+      return <ComposedComponent {...this.props} />
+    }
+  }
+
+  function mapStateToProps(state) {
+    return { authenticated: state.authenticated };
+  }
+
+  return connect(mapStateToProps)(Authentication);
 }
